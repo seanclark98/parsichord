02dd5e0a0c54012988085bf5dca9098532ae6b15
Sean Clark <seanclark98@hotmail.co.uk>
2022-09-14 14:25:03 +0100

    add create superuser method to user model

--- parsichord/core/models.py
+++ parsichord/core/models.py
@@ -15,16 +15,21 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
         if not email:
-            raise ValueError('User must have an email address.')
+            raise ValueError("User must have an email address.")
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
+    def create_superuser(self, email, password):
+        """Create, save and return a new superuser."""
+        return self.create_user(email, password, is_superuser=True, is_staff=True)
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
+
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
@@ -32,4 +37,4 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
